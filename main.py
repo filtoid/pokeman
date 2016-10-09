95bd17062d90f130562707c79f751ab12dfd97a7
Phil Jeffes <philjeffes@hotmail.com>
2016-10-09 22:41:45 +0100

    Add in keyboard input allow ball to move

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 ball = None
-speed = [2, 2]
+speed = [0, 0]
 
 def update(size):
     global ball
@@ -9,9 +9,9 @@ def update(size):
     ball['rect'] = ball['rect'].move(speed)
 
     if ball['rect'].left < 0 or ball['rect'].right > size[0]:
-        speed[0] = -speed[0]
+        ball['rect'] = ball['rect'].move([speed[0]*-1,0])
     if ball['rect'].top < 0 or ball['rect'].bottom > size[1]:
-        speed[1] = -speed[1]
+        ball['rect'] = ball['rect'].move([0,speed[1]*-1])
 
 
 def draw(screen):
@@ -27,6 +27,8 @@ def draw(screen):
 
 def main():
     global ball
+    global speed
+
     ball = {}
     ball['image'] = pygame.image.load("pokeball_closed.png")
     ball['rect'] = ball['image'].get_rect()
@@ -42,6 +44,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit = True
+        keys = pygame.key.get_pressed()
+        #Reset speed then adjust based on which keys are pressed
+        speed = [0,0]
+        if keys[pygame.K_LEFT]:
+            speed[0] -= 4
+        if keys[pygame.K_RIGHT]:
+            speed[0] += 4
+        if keys[pygame.K_UP]:
+            speed[1] -= 4
+        if keys[pygame.K_DOWN]:
+            speed[1] += 4
+
+        if keys[pygame.K_ESCAPE]:
+            quit = True
+
         clock.tick(30)
         update(size)
         draw(screen)
